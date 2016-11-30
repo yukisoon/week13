0b8078572b8e3425bf3fd3dc94879f0427f154f3
yukisoon <ykscj39@google.com>
2016-11-30 14:21:27 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,245 @@
+/*
+
+http://www.opengl-tutorial.org/beginners-tutorials/
+https://github.com/opengl-tutorials/ogl
+
+*/
+
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <vector>
+
+using namespace glm;
+
+#include "shader.hpp"
+
+int main(void)
+{
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// white background color
+	glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	// Create and compile our GLSL program from the shaders
+	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+
+	//const GLfloat triangle_vertices[] = {
+	//	0.0f, 0.0f, 0.0f,
+	//	1.0f, 0.0f, 0.0f,
+	//	0.5f,  1.0f, 0.0f,
+	//};
+	/*std::vector<GLfloat> triangle_vertices;
+	triangle_vertices.push_back(0.0f);
+	triangle_vertices.push_back(0.0f);
+	triangle_vertices.push_back(0.0f);
+	triangle_vertices.push_back(1.0f);
+	...*/
+
+	std::vector<glm::vec3> triangle_vertices;
+	triangle_vertices.push_back(glm::vec3(0.0f, 0.0f, 0.0f));
+	triangle_vertices.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+	triangle_vertices.push_back(glm::vec3(0.5f, 1.0f, 0.0f));
+
+	glm::vec3 tri_center(0.0f);
+	for (int i = 0; i < triangle_vertices.size(); i++)
+	{
+		tri_center += triangle_vertices[i];
+	}
+	tri_center /= triangle_vertices.size();
+
+	const GLfloat square_vertices[] = {
+		0.0f, 0.0f, 0.0f,
+		1.0f, 0.0f, 0.0f,
+		1.0f, 1.2f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+
+	glm::vec3 dir(0.0f, 1.0f, 0.0f);
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, triangle_vertices.size() * sizeof(glm::vec3),
+		&(triangle_vertices[0].x), GL_STATIC_DRAW);
+
+	GLuint vertexbuffer2;
+	glGenBuffers(1, &vertexbuffer2);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(square_vertices), square_vertices, GL_STATIC_DRAW);
+
+	glm::mat4 Model = glm::mat4(1.0f);
+
+	do {
+
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+
+		GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+		glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+		//glm::mat4 Projection = glm::ortho(-1.0f,1.0f,-1.0f,1.0f,0.0f,100.0f); // In world coordinates
+
+		// Camera matrix
+		glm::mat4 View = glm::lookAt(
+			glm::vec3(0.5, 0.5, 3), // Camera is at (4,3,3), in World Space
+			glm::vec3(0.5, 0.5, 0), // and looks at the origin
+			glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+		);
+
+		//Model = glm::translate(Model, glm::vec3(0.0, 0.1f, 0.0f));
+		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		{
+			// rotation about the origin
+		//	Model = glm::rotate(Model, glm::radians(1.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+
+			const float turn_coeff_ = 1.0f;
+
+			// rotation about the center of the triangle
+			Model = glm::translate(+tri_center) * glm::rotate(glm::mat4(), glm::radians(turn_coeff_), glm::vec3(0.0f, 0.0f, 1.0f)) * glm::translate(-tri_center) * Model;
+
+			// rotate dir vector
+			const glm::mat4 rot_mat = glm::rotate(glm::mat4(), glm::radians(turn_coeff_), glm::vec3(0, 0, 1));
+
+			glm::vec4 temp(dir.x, dir.y, dir.z, 0.0f);
+
+			temp = rot_mat * temp;
+
+			dir.x = temp.x;
+			dir.y = temp.y;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		{
+			const float turn_coeff_ = -1.0f;
+			// rotate model matrix 
+			Model = glm::translate(+tri_center) * glm::rotate(glm::mat4(), glm::radians(turn_coeff_), glm::vec3(0.0f, 0.0f, 1.0f)) * glm::translate(-tri_center) * Model;
+			
+			// rotate dir vector
+			const glm::mat4 rot_mat = glm::rotate(glm::mat4(), glm::radians(turn_coeff_), glm::vec3(0, 0, 1));
+
+			glm::vec4 temp(dir.x, dir.y, dir.z, 0.0f);
+
+			temp = rot_mat * temp;
+
+			dir.x = temp.x;
+			dir.y = temp.y;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		{
+			// translate 
+			Model = glm::translate(Model, glm::vec3(0.0f, 0.01f, 0.0f));
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		{
+			// move backward
+			Model = glm::translate(Model, glm::vec3(0.0f, -0.01f, 0.0f));
+		}
+
+		glm::mat4 MVP = Projection * View * Model;
+
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size (x, y, z)
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_LINE_LOOP, 0, 3); // 3 vertices
+
+
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer2);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		glDrawArrays(GL_LINE_LOOP, 0, 4); // 4 vertices
+		//glDrawArrays(GL_LINES, 0, 4);
+
+		glDisableVertexAttribArray(0);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+
+	// Cleanup VBO
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteProgram(programID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
